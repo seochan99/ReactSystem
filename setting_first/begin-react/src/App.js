91c2530e0d28da8e29ccd686a48ddda04fa6d0f9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-04-30 12:54:54 +0900

    feat : 4/30 tutoring

---
+++ setting_first/begin-react/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Counter from "./component/counter";
+import Hello from "./component/hello";
+
+function App() {
+    return (
+        <div>
+            <Hello />
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
---
+++ setting_first/begin-react/src/component/counter.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react'
+
+export default function Counter() {
+    
+    const [count, setCount] = useState(0);
+
+    const onIncrease = () => {
+
+        setCount(count+1);
+        // setCount(prevCount => prevCount + 1);
+    };
+    const onDecrease = () => {
+        count = count - 1;
+        // setCount(count-1);
+        // setCount(prevCount => prevCount - 1);
+    };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  )
+}
